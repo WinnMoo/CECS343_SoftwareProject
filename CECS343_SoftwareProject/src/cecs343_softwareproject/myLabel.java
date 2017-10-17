d25b912bcf804a9c9642ba413abbd7f304e2d500
scubaastev <scubaastev@gmail.com>
2017-10-16 22:54:25 -0700

    Added Class myLabel for tokens

---
+++ CECS343_SoftwareProject/src/cecs343_softwareproject/myLabel.java
@@ -0,0 +1,46 @@
+package cecs343_softwareproject;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class myLabel
+  extends JLabel
+{
+  static View m;
+  static Model model;
+  
+  public myLabel(ImageIcon i)
+  {
+    super(i);
+    model = Controller.getModel();
+  }
+  
+  public void paintComponent(Graphics g)
+  {
+    Graphics2D g2d = (Graphics2D)g;
+    super.paintComponent(g2d);
+    g2d.setColor(Color.RED);
+    g2d.setFont(new Font("Monospaced", 1, 40));
+    
+    g2d.drawString(model.listOfPlayers[0].getName(), getRoomX(0), getRoomY(0));
+    g2d.drawString(model.listOfPlayers[1].getName(), getRoomX(1), getRoomY(1));
+    g2d.drawString(model.listOfPlayers[2].getName(), getRoomX(2), getRoomY(2));
+  }
+  
+  private int getRoomX(int i)
+  {
+    int room = model.listOfPlayers[i].roomNum;
+    return ((Room)model.rooms.get(room)).x;
+  }
+  
+  private int getRoomY(int i)
+  {
+    int room = model.listOfPlayers[i].roomNum;
+    return ((Room)model.rooms.get(room)).y + i * 40 + 40;
+  }
+}
