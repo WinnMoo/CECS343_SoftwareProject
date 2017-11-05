a3def3d1d6b0f4c2d17e708232dc4be721160bcb
scubaastev <scubaastev@gmail.com>
2017-11-05 00:46:38 -0700

    Added components of button panel
    needs work to make it exact, having information panel be 2/3s

--- CECS343_SoftwareProject/src/cecs343_softwareproject/View.java
+++ CECS343_SoftwareProject/src/cecs343_softwareproject/View.java
@@ -20,124 +20,185 @@ import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import cecs343_softwareproject.Controller.MyJButton;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
+import java.awt.TextArea;
+import java.awt.TextField;
 import java.util.ArrayList;
 
 public class View extends javax.swing.JPanel {
 
-    private JFrame frame;
+	private JFrame frame;
 
-    //Gameboard Panel
-    public JPanel imagePanel;
-    private ImageIcon gameBoard;
-    private JLabel imageLabel;
-    public JLabel token1;
-    public JLabel token2;
-    public JLabel token3;
-    private JLabel token4;
-    //Controls Panel
-    private JPanel buttonPanel;
-    private MyJButton moveButton;
-    public JList roomsList;
-    public DefaultListModel listModel;
-    public Model appModel;
-    private Room listOfRooms;
+	// Gameboard Panel
+	public JPanel imagePanel;
+	private ImageIcon gameBoard;
+	private JLabel imageLabel;
+	public JLabel token1;
+	public JLabel token2;
+	public JLabel token3;
+	private JLabel token4;
+	// Controls Panel
+	private JPanel buttonPanel;
+	private MyJButton moveButton;
+	public JList roomsList;
+	public DefaultListModel listModel;
+	public Model appModel;
+	private Room listOfRooms;
 
- 
+	private JScrollPane mapScroller;
 
-    private JScrollPane mapScroller;
+	private JScrollPane roomListScroller;
 
-    private JScrollPane roomListScroller;
+	private MyJButton playCardButton;
 
-    public View(Controller appController) {
-        //Gameboard Components
-        gameBoard = new ImageIcon("CSULBMap5_1200x1437.png");
-        imageLabel = new JLabel("", gameBoard, JLabel.CENTER);
-        imagePanel = new JPanel(new BorderLayout());
+	private MyJButton drawCardButton;
 
-        mapScroller = new JScrollPane(imagePanel);
-        
+	private ImageIcon card1;
 
-         imageLabel.setLayout(null);
-         
-         token1 = new JLabel("John");
-         
-         token1.setFont(token1.getFont().deriveFont(Font.BOLD, 20));
-         token1.setForeground(Color.RED);
-         token1.setBounds(appController.appModel.listOfPlayers[0].room.getRoomPositionX(),appController.appModel.listOfPlayers[0].room.getRoomPositionY(), 100, 50);
+	private JLabel cardLabel;
 
-         
-         token2 = new JLabel("Rebecca");
-         token2.setFont(token2.getFont().deriveFont(Font.BOLD, 20));
-         token2.setForeground(Color.RED);
-         token2.setBounds(appController.appModel.listOfPlayers[1].room.getRoomPositionX(),appController.appModel.listOfPlayers[1].room.getRoomPositionY()+25, 100, 50);
+	private TextField textField;
 
-       
-         
-         token3 = new JLabel("Charles");
-         token3.setFont(token3.getFont().deriveFont(Font.BOLD, 20));
-         token3.setForeground(Color.RED);
-         token3.setBounds(appController.appModel.listOfPlayers[2].room.getRoomPositionX(),appController.appModel.listOfPlayers[2].room.getRoomPositionY()-25, 100, 50);
-         
-         
-         
-         token4 = new JLabel("Jamal");
-         token4.setFont(token4.getFont().deriveFont(Font.BOLD, 24));
-         token4.setForeground(Color.RED);
-         token4.setBounds(1400,800, 100, 50);
+	private JPanel leftButtonPanel;
 
-         imagePanel.add(token1);
-         imagePanel.add(token2);
-         imagePanel.add(token3);
-         imagePanel.add(token4);
-        
-       
-        //Controls Components
-     mapScroller = new JScrollPane(imagePanel);
+	private JPanel rightButtonPanel;
 
-        //Controls Components
-        DefaultListModel listModel = new DefaultListModel();
-        for (int i = 0; i < appController.appModel.createListOfRooms().length; i++) {
-            listModel.addElement(appController.appModel.createListOfRooms()[i]);
-        }
-        roomsList = new JList(listModel);
-        roomsList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
-        roomsList.setLayoutOrientation(JList.VERTICAL);
-        roomsList.setVisibleRowCount(5);
+	private JPanel middleButtonPanel;
 
-        roomListScroller = new JScrollPane(roomsList);
+	private TextArea textArea;
 
-        buttonPanel = new JPanel();
-        moveButton = appController.new MyJButton("Move");
+	public View(Controller appController) {
+		// Gameboard Components
+		gameBoard = new ImageIcon("CSULBMap5_1200x1437.png");
+		card1 = new ImageIcon("card1.png");
+		cardLabel = new JLabel("",card1, JLabel.CENTER);
+		imageLabel = new JLabel("", gameBoard, JLabel.CENTER);
+		imagePanel = new JPanel(new BorderLayout());
 
-        frame = new JFrame();
+		mapScroller = new JScrollPane(imagePanel);
 
-        
-      
-    }
-       
- 
+		imageLabel.setLayout(null);
 
-    void displayUI() {
-       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setUndecorated(true);
+		token1 = new JLabel("John");
 
-        //imagePanel.setPreferredSize(new Dimension(1200, 949));
-        roomListScroller.setPreferredSize(new Dimension(120, 150));
+		token1.setFont(token1.getFont().deriveFont(Font.BOLD, 20));
+		token1.setForeground(Color.RED);
+		token1.setBounds(appController.appModel.listOfPlayers[0].room.getRoomPositionX(),
+				appController.appModel.listOfPlayers[0].room.getRoomPositionY(), 100, 50);
 
-        imagePanel.add(imageLabel, BorderLayout.NORTH);
+		token2 = new JLabel("Rebecca");
+		token2.setFont(token2.getFont().deriveFont(Font.BOLD, 20));
+		token2.setForeground(Color.RED);
+		token2.setBounds(appController.appModel.listOfPlayers[1].room.getRoomPositionX(),
+				appController.appModel.listOfPlayers[1].room.getRoomPositionY() + 25, 100, 50);
 
-        buttonPanel.add(roomListScroller);
-        buttonPanel.add(moveButton);
+		token3 = new JLabel("Charles");
+		token3.setFont(token3.getFont().deriveFont(Font.BOLD, 20));
+		token3.setForeground(Color.RED);
+		token3.setBounds(appController.appModel.listOfPlayers[2].room.getRoomPositionX(),
+				appController.appModel.listOfPlayers[2].room.getRoomPositionY() - 25, 100, 50);
 
-        frame.add(buttonPanel, BorderLayout.SOUTH);
-        //Only need to add the scrollpanel because the image panel is added to the scroll panel
-        //Scrollpanel automatically scales the image according to the preferred size
-        frame.add(mapScroller);
-        frame.setVisible(true);
-    }
+		token4 = new JLabel("Jamal");
+		token4.setFont(token4.getFont().deriveFont(Font.BOLD, 24));
+		token4.setForeground(Color.RED);
+		token4.setBounds(1400, 800, 100, 50);
 
+		imagePanel.add(token1);
+		imagePanel.add(token2);
+		imagePanel.add(token3);
+		imagePanel.add(token4);
+
+		// Controls Components
+		mapScroller = new JScrollPane(imagePanel);
+
+		// Controls Components
+		DefaultListModel listModel = new DefaultListModel();
+		for (int i = 0; i < appController.appModel.createListOfRooms().length; i++) {
+			listModel.addElement(appController.appModel.createListOfRooms()[i]);
+		}
+		roomsList = new JList(listModel);
+		roomsList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+		roomsList.setLayoutOrientation(JList.VERTICAL);
+		roomsList.setVisibleRowCount(5);
+		roomListScroller = new JScrollPane(roomsList);
+		buttonPanel = new JPanel();
+		leftButtonPanel = new JPanel();
+		middleButtonPanel= new JPanel();
+		rightButtonPanel = new JPanel();
+		drawCardButton = appController.new MyJButton("Draw Card");
+		moveButton = appController.new MyJButton("Move");
+		playCardButton = appController.new MyJButton("Play Card");
+		
+		
+		
+		//TextField
+		textField = new java.awt.TextField();
+		textField.setText("HELLO");
+
+		textArea = new java.awt.TextArea();
+		textArea.setText("AMANDA WAS HERE");
+		
+		frame = new JFrame();
+
+	}
+
+	void displayUI() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new BorderLayout());
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		frame.setUndecorated(true);
+
+		// imagePanel.setPreferredSize(new Dimension(1200, 949));
+		roomListScroller.setPreferredSize(new Dimension(100, 100));
+		// roomListScroller.setBounds(100, 1000, 150, 150);;
+		imagePanel.add(imageLabel, BorderLayout.NORTH);
+		// buttonPanel.setBounds(40,1300,100,60);
+		leftButtonPanel.setLayout(new BoxLayout(leftButtonPanel, BoxLayout.Y_AXIS));
+		
+		drawCardButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		leftButtonPanel.add(drawCardButton);
+		
+		moveButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		leftButtonPanel.add(moveButton);
+		
+		playCardButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+		leftButtonPanel.add(playCardButton);
+		
+		roomListScroller.setAlignmentX(Component.CENTER_ALIGNMENT);
+		leftButtonPanel.add(roomListScroller);
+		
+		buttonPanel.add( Box.createVerticalStrut(1));
+
+//		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+		middleButtonPanel.add(cardLabel);
+		buttonPanel.add( Box.createVerticalStrut(1));
+		rightButtonPanel.setLayout(new BoxLayout(rightButtonPanel, BoxLayout.Y_AXIS));
+
+		rightButtonPanel.add(textField);
+		rightButtonPanel.add(textArea);
+		
+		
+		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+
+		buttonPanel.add(leftButtonPanel);
+		leftButtonPanel.setAlignmentX(Component.BOTTOM_ALIGNMENT);
+
+		buttonPanel.add(middleButtonPanel);
+		leftButtonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+		buttonPanel.add(rightButtonPanel);
+		leftButtonPanel.setAlignmentX(Component.RIGHT_ALIGNMENT);
+
+
+
+		frame.add(buttonPanel, BorderLayout.SOUTH);
+		// Only need to add the scrollpanel because the image panel is added to
+		// the scroll panel
+		// Scrollpanel automatically scales the image according to the preferred
+		// size
+		frame.add(mapScroller);
+		frame.setVisible(true);
+	}
 
 }
